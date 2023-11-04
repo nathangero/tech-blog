5cb78182fca222e39f5f61636bc636e7a95c83ac
nathangero <nathangero@yahoo.com>
2023-11-03 18:10:46 -0700

    add  comments

--- models/index.js
+++ models/index.js
@@ -1,16 +1,10 @@
-// All models are 1 to many. NO many to many
-/*
-User have many posts
-Post have many comments
-Comments belong to many Users and Posts
-
-*/
-
 const User = require("./User");
 const Post = require("./Post");
 const Comment = require("./Comment");
 
 // Establish relationships
+
+// User has many Posts and Comments
 User.hasMany(Post, {
     foreignKey: "user_id",
     onDelete: 'CASCADE'
@@ -21,6 +15,7 @@ User.hasMany(Comment, {
     onDelete: 'CASCADE'
 })
 
+// Post has many Comments
 Post.hasMany(Comment, {
     foreignKey: "post_id",
     onDelete: 'CASCADE'
