66891f8ec3ec5406c12c9bdddba200e8edc06e01
nathangero <nathangero@yahoo.com>
2023-11-03 20:02:13 -0700

    implemented getting a single user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../../models");
 
-
+// Get all users. Probably not used but implemented anyway
 router.get("/", async (req, res) => {
     try {
         const data = await User.findAll({
-            // includes: [{ model: Post }, { model: Comment }]
-        })
+            include: [{ model: Post }, { model: Comment }] // Show all posts and comments of all users
+        });
 
         if (data.length > 0) {
             const users = data.map((element) => element.get({ plain: true }));
@@ -20,5 +20,32 @@ router.get("/", async (req, res) => {
         res.status(500).json(error);
     }
 })
+
+// Get one user given an id
+router.get("/:id", async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const data = await User.findByPk(userId, {
+            include: [{ model: Post }, { model: Comment }] // Show all posts and comments of a user
+        })
+
+        if (data) {
+            const user = data.get({ plain: true });
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: "There are no users in the database"});
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+
+// Create new user
+router.post("/", async (req, res) => {
+    
+})
+
 
 module.exports = router;
