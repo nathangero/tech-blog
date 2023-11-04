45a7d83856a1a5233240b8840db4f60cc56e36d2
nathangero <nathangero@yahoo.com>
2023-11-04 13:12:21 -0700

    implement showing the date a post was created

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,24 @@
 module.exports = {
-    
+    formatDate(date) {
+        console.log("date:", date);
+        const months = {
+            0: "Jan",
+            1: "Feb",
+            2: "Mar",
+            3: "Apr",
+            4: "May",
+            5: "June",
+            6: "July",
+            7: "Aug",
+            8: "Sept",
+            9: "Oct",
+            10: "Nov",
+            11: "Dec"
+        }
+        const day = date.getDate();
+        const month = months[date.getMonth()];
+        const year = date.getFullYear();
+
+        return `${month} ${day}, ${year}`
+    }
 }
