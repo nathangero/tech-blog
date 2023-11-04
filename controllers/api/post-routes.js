48487d19d3fdfae035ae5163c907d518844e8ae8
nathangero <nathangero@yahoo.com>
2023-11-03 21:03:42 -0700

    implement creating a post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -71,5 +71,16 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+// Create a new post
+router.post("/", async (req, res) => {
+    try {
+        const data = await Post.create(req.body);
+        
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
 
 module.exports = router;
