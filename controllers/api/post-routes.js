ee22b58744a340b18664186edd6ff034d6a0cbd3
nathangero <nathangero@yahoo.com>
2023-11-03 20:50:39 -0700

    implement getting all posts and their poster and comments

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -6,7 +6,38 @@
     delete post
 */
 const router = require("express").Router();
+const sequelize = require("../../config/connection");
 const { User, Post, Comment } = require("../../models");
+
+
+router.get("/", async (req, res) => {
+    try {
+        const data = await Post.findAll({
+            include: [{ model: Comment }],
+            attributes: {
+                include: [
+                    [
+                        sequelize.literal(
+                            `(SELECT user.username FROM user WHERE post.user_id = user.id)`
+                        ),
+                        "posters_name",
+                    ]
+                ]
+            }
+        });
+
+        if (data.length > 0) {
+            const posts = data.map((element) => element.get({ plain: true }));
+            // console.log("users:", users)
+            res.status(200).json(posts);
+        } else {
+            res.status(404).json({ message: "There are no posts in the database"});
+        }
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
 
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -8,18 +8,18 @@ const Comment = require("./Comment");
 User.hasMany(Post, {
     foreignKey: "user_id",
     onDelete: 'CASCADE'
-})
+});
 
 User.hasMany(Comment, {
     foreignKey: "user_id",
     onDelete: 'CASCADE'
-})
+});
 
 // Post has many Comments
 Post.hasMany(Comment, {
     foreignKey: "post_id",
     onDelete: 'CASCADE'
-})
+});
 
 
 module.exports = { User, Post, Comment }
