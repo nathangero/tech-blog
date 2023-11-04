22d5f42130ecd9de113ec2490211589d5d0ccb1d
nathangero <nathangero@yahoo.com>
2023-11-03 21:01:02 -0700

    implement getting a single post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -9,7 +9,7 @@ const router = require("express").Router();
 const sequelize = require("../../config/connection");
 const { User, Post, Comment } = require("../../models");
 
-
+// Get all posts, its comments, and the username that posted it
 router.get("/", async (req, res) => {
     try {
         const data = await Post.findAll({
@@ -37,7 +37,39 @@ router.get("/", async (req, res) => {
     } catch (error) {
         res.status(500).json(error);
     }
-})
+});
+
+// Get one post given an id
+router.get("/:id", async (req, res) => {
+    const postId = req.params.id;
+    
+    try {
+        const data = await Post.findByPk(postId, {
+            include: [{ model: Comment }],
+            attributes: {
+                include: [
+                    [
+                        sequelize.literal(
+                            `(SELECT user.username FROM user WHERE post.user_id = user.id)`
+                        ),
+                        "posters_name",
+                    ]
+                ]
+            }
+        });
+
+        if (data) {
+            const posts = data.get({ plain: true });
+            // console.log("users:", users)
+            res.status(200).json(posts);
+        } else {
+            res.status(404).json({ message: `Post ${postId} doesn't exist`});
+        }
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 
 module.exports = router;
